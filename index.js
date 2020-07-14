b3a3ef71fee7fa264a82eab61a7936877f328379
Angelo Martinez <amart670@fiu.edu>
2020-07-13 23:21:04 -0400

    added night mode switch

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-document
-  .querySelector('.js-switch-wrapper')
-  .addEventListener('click', function (e) {
-    console.log('hello');
-  });
+let darkTheme = false;
+const switchWrapper = document.querySelector('.js-switch-wrapper');
+const switcher = document.querySelector('.js-switch');
+
+switchWrapper.addEventListener('click', () => {
+  if (darkTheme === false) {
+    document
+      .querySelector(':root')
+      .style.setProperty('--theme-color-light', '#090629');
+    document
+      .querySelector(':root')
+      .style.setProperty('--theme-color-dark', '#08061f');
+    switcher.classList.add('on');
+  } else {
+    document
+      .querySelector(':root')
+      .style.setProperty('--theme-color-light', 'rgba(255, 236, 210, 1)');
+    document
+      .querySelector(':root')
+      .style.setProperty('--theme-color-dark', 'rgba(252, 182, 159, 0.51)');
+    switcher.classList.remove('on');
+  }
+  darkTheme = !darkTheme;
+});
